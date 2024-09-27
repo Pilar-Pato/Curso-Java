1befacfd64f0fdd3a81fb4353bc21f325def88f2
PilarPato <fusionilusion@gmail.com>
2024-09-27 21:39:24 +0200

    Finalizado

---
+++ Variables11.java
@@ -0,0 +1,15 @@
+public class Variables11 {
+
+    public static void main(String[] args) {
+
+//11. Operaciones matemáticas:Math.pow(), Math.sqrt(), y Math.abs().
+
+        double potencia =Math.pow(2, 3);
+        System.out.println(potencia);
+        double raiz = Math.sqrt(49);
+        System.out.println(raiz);
+        double ab =Math.abs(-100);
+        System.out.println(ab);
+
+    }
+}
